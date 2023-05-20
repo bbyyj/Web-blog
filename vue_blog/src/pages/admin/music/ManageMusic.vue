2ade2a08a35838d7e1e7c0c267944913751a283e
QYT <76135896@qq.com>
2023-05-20 20:52:58 +0800

    添加后台管理增删音乐前端

---
+++ vue_blog/src/pages/admin/music/ManageMusic.vue
@@ -0,0 +1,156 @@
+<template>
+    <div>
+        <el-card>
+            <el-row>
+                <el-button class="add" type="primary" plain @click="handleAdd">添加音乐</el-button>
+            </el-row>
+            <el-table :data="musicList" border stripe>
+                <el-table-column type="index"></el-table-column>
+                <el-table-column label="name" prop="name" width="300px"></el-table-column>
+                <el-table-column label="artist" prop="artist"></el-table-column>
+                <el-table-column label="url" prop="url" width="180px"></el-table-column>
+                <el-table-column label="cover" prop="cover"></el-table-column>
+                <el-table-column>
+                    <template slot-scope="scope">
+                        <el-button size="mini" type="danger" @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+                    </template>
+                </el-table-column>
+            </el-table>
+
+            <el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange"
+                :current-page="queryInfo.pageNum" :page-sizes="[10, 12, 15, 20]" :page-size="queryInfo.pageSize"
+                layout="total, sizes, prev, pager, next, jumper" :total="total">
+            </el-pagination>
+
+            <el-dialog title="音乐信息" :visible.sync="dialogFormVisible">
+                <el-form label-width="80px">
+                    <el-form-item label="name:">
+                        <el-input v-model="postInfo.name" autocomplete="off" clearable></el-input>
+                    </el-form-item>
+                    <el-form-item label="artist:">
+                        <el-input v-model="postInfo.artist" autocomplete="off" clearable></el-input>
+                    </el-form-item>
+                    <el-form-item label="url:">
+                        <el-input v-model="postInfo.url" autocomplete="off" clearable></el-input>
+                    </el-form-item>
+                    <el-form-item label="cover:">
+                        <el-input v-model="postInfo.cover" autocomplete="off" clearable></el-input>
+                    </el-form-item>
+                </el-form>
+                <div slot="footer" class="dialog-footer">
+                    <el-button @click="cancel">取 消</el-button>
+                    <el-button type="primary" @click="commitMusic">确 定</el-button>
+                </div>
+            </el-dialog>
+        </el-card>
+    </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+    name: "manageMusic",
+    data() {
+        return {
+            musicList: [],
+            dialogFormVisible: false,
+            total: 0,
+            postInfo: {
+                id: 0,
+                name: "",
+                artist: "",
+                url: "",
+                cover: "",
+            },
+            queryInfo: {
+                pageNum: 1,
+                pageSize: 10
+            },
+        }
+    },
+    methods: {
+        async getMusicList() {
+            const { data: res } = await this.$axios.get("/admin/musicList", {
+                params: {
+                    pageNum: this.queryInfo.pageNum,
+                    pageSize: this.queryInfo.pageSize
+                }
+            });
+            if (res.status === 1) {
+                this.musicList = res.data.data;
+                this.total = res.data.total;
+            } else {
+                this.$message.error("获取音乐列表失败，请重试！")
+            }
+        },
+        async handleAdd() {
+            this.postInfo.id = 0
+            this.dialogFormVisible = true
+        },
+        async handleDelete(id) {
+            this.$messageBox.confirm('确认删除该音乐?', '提示', {
+                confirmButtonText: '确定',
+                cancelButtonText: '取消',
+                type: 'warning'
+            }).then(async () => {
+                const { data: res } = await this.$axios.delete("/admin/deleteMusic", { params: { id: id } });
+                if (res.status !== 1) {
+                    this.$message.error("删除失败，请重试！")
+                } else {
+                    this.$message.success("删除成功！")
+                    await this.getMusicList();
+                }
+            }, () => {
+                this.$message({ type: 'info', message: '已取消删除' });
+            });
+        },
+
+        handleSizeChange: function (pagesize) {
+            this.queryInfo.pageSize = pagesize;
+            this.getMusicList();
+        },
+        handleCurrentChange: function (newPage) {
+            this.queryInfo.pageNum = newPage;
+            this.getMusicList();
+        },
+
+        cancel() {
+            this.postInfo = {
+                id: 0,
+                name: "",
+                artist: "",
+                url: "",
+                cover: "",
+            }
+            this.dialogFormVisible = false
+        },
+
+        async commitMusic() {
+            let res
+            if (this.postInfo.id === 0) {
+                res = await this.$axios.post("/admin/addMusic", this.postInfo)
+            } else {
+                res = await this.$axios.put("/admin/updateMusic", this.postInfo)
+            }
+            if (res.data.status !== 1) {
+                this.$message.error("操作失败，请重试！")
+            } else {
+                this.cancel()
+                await this.getMusicList()
+                this.$message.success("操作成功！")
+            }
+        },
+    },
+    created() {
+        this.getMusicList()
+    }
+}
+</script>
+
+<style scoped>
+.add {
+    float: right !important;
+    margin-right: 50px;
+}
+</style>
--- vue_blog/src/router/index.js
+++ vue_blog/src/router/index.js
@@ -1,87 +1,89 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
+import Vue from "vue";
+import VueRouter from "vue-router";
 
+const Index = () => import("../pages");
+const Home = () => import("../pages/Home.vue");
+const BlogDetail = () => import("../pages/BlogDetail.vue");
+const Types = () => import("../pages/Types.vue");
+const Tags = () => import("../pages/Tags.vue");
+const TimeLine = () => import("../pages/TimeLine.vue");
+const About = () => import("../pages/About.vue");
+const Essay = () => import("../pages/Essay");
+const MsgBoard = () => import("../pages/MsgBoard");
+const PhotoWall = () => import("../pages/PhotoWall");
+const ResourceLib = () => import("../pages/ResourceLib");
+const NotFound = () => import("../components/error/NOTFOUND.vue");
+const InternalError = () => import("../components/error/INTERNALERROR.vue");
 
-const Index = () => import( "../pages")
-const Home = () => import( "../pages/Home.vue")
-const BlogDetail = () => import( "../pages/BlogDetail.vue")
-const Types = () => import( "../pages/Types.vue")
-const Tags = () => import( "../pages/Tags.vue")
-const TimeLine = () => import( "../pages/TimeLine.vue")
-const About = () => import( "../pages/About.vue")
-const Essay = () => import( "../pages/Essay")
-const MsgBoard = () => import( "../pages/MsgBoard")
-const PhotoWall = () => import( "../pages/PhotoWall")
-const ResourceLib = () => import( "../pages/ResourceLib")
-const NotFound = () => import( "../components/error/NOTFOUND.vue")
-const InternalError = () => import( "../components/error/INTERNALERROR.vue")
+const Login = () => import("../pages/admin/Login");
+const AdminHome = () => import("../pages/admin/Home");
+const Welcome = () => import("../pages/admin/Welcome.vue");
+const ListBlogs = () => import("../pages/admin/blogs/ListBlogs.vue");
+const ListTypes = () => import("../pages/admin/types/ListTypes.vue");
+const ListTags = () => import("../pages/admin/tags/ListTags.vue");
+const AddBlog = () => import("../pages/admin/blogs/AddBlog.vue");
+const ListImages = () => import("../pages/admin/images/ListImages");
+const ManageMessage = () => import("../pages/admin/messages/ManageMessage");
+const ListMottos = () => import("../pages/admin/blogs/listMottos");
+const ManageResLink = () => import("../pages/admin/resourecLink/manageResLink");
+const ManageMusic = () => import("../pages/admin/music/ManageMusic");
 
-
-const Login = () => import("../pages/admin/Login")
-const AdminHome  = () => import('../pages/admin/Home')
-const Welcome  = () => import( "../pages/admin/Welcome.vue")
-const ListBlogs  = () => import( "../pages/admin/blogs/ListBlogs.vue")
-const ListTypes  = () => import( "../pages/admin/types/ListTypes.vue")
-const ListTags  = () => import( "../pages/admin/tags/ListTags.vue")
-const AddBlog  = () => import( "../pages/admin/blogs/AddBlog.vue")
-const ListImages  = () => import( "../pages/admin/images/ListImages")
-const ManageMessage  = () => import( "../pages/admin/messages/ManageMessage")
-const ListMottos  = () => import( "../pages/admin/blogs/listMottos")
-const ManageResLink  = () => import( "../pages/admin/resourecLink/manageResLink")
-const ManageEssay  = () => import( "../pages/admin/essay/ManageEssay")
-
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
     component: Index,
     meta: {
-        auth: false
+      auth: false,
     },
     redirect: "/home",
     children: [
-      { path: "/home", component: Home, meta: {auth: false} },
-      { path: "/blogDetail", component: BlogDetail, meta: {auth: false} },
-      { path: "/types", component: Types, meta: {auth: false} },
-      { path: "/tags", component: Tags, meta: {auth: false} },
-      { path: "/timeLine", component: TimeLine, meta: {auth: false} },
-      { path: "/essay", component: Essay, meta: {auth: false} },
-      { path: "/msgBoard", component: MsgBoard, meta: {auth: false} },
-      { path: "/photoWall", component: PhotoWall, meta: {auth: false} },
-      { path: "/resourceLib", component: ResourceLib, meta: {auth: false} },
-      { path: "/about", component: About, meta: {auth: false} },
-      { path: "/notFound", component: NotFound, meta: {auth: false} },
-      { path: "/internalError", component: InternalError, meta: {auth: false} }
-    ]
+      { path: "/home", component: Home, meta: { auth: false } },
+      { path: "/blogDetail", component: BlogDetail, meta: { auth: false } },
+      { path: "/types", component: Types, meta: { auth: false } },
+      { path: "/tags", component: Tags, meta: { auth: false } },
+      { path: "/timeLine", component: TimeLine, meta: { auth: false } },
+      { path: "/essay", component: Essay, meta: { auth: false } },
+      { path: "/msgBoard", component: MsgBoard, meta: { auth: false } },
+      { path: "/photoWall", component: PhotoWall, meta: { auth: false } },
+      { path: "/resourceLib", component: ResourceLib, meta: { auth: false } },
+      { path: "/about", component: About, meta: { auth: false } },
+      { path: "/notFound", component: NotFound, meta: { auth: false } },
+      {
+        path: "/internalError",
+        component: InternalError,
+        meta: { auth: false },
+      },
+    ],
   },
   { path: "/login", component: Login },
   {
     path: "/manageHome",
     component: AdminHome,
     meta: {
-        auth: false
+      auth: false,
     },
     redirect: "/welcome",
     children: [
-      { path: "/welcome", component: Welcome, meta: {auth: true} },
-      { path: "/listBlogs", component: ListBlogs, meta: {auth: true} },
-      { path: "/addBlog", component: AddBlog, meta: {auth: true} },
-      { path: "/listMottos", component: ListMottos, meta: {auth: true} },
-      { path: "/listTypes", component: ListTypes, meta: {auth: true} },
-      { path: "/listTags", component: ListTags, meta: {auth: true} },
-      { path: "/listImages", component: ListImages, meta: {auth: true} },
-      { path: "/manageMsg", component: ManageMessage, meta: {auth: true} },
-      { path: "/manageLink", component: ManageResLink, meta: {auth: true} },
-      { path: "/manageEssay", component: ManageEssay, meta: {auth: true} }
-    ]
-  }
-]
+      { path: "/welcome", component: Welcome, meta: { auth: true } },
+      { path: "/listBlogs", component: ListBlogs, meta: { auth: true } },
+      { path: "/addBlog", component: AddBlog, meta: { auth: true } },
+      { path: "/listMottos", component: ListMottos, meta: { auth: true } },
+      { path: "/listTypes", component: ListTypes, meta: { auth: true } },
+      { path: "/listTags", component: ListTags, meta: { auth: true } },
+      { path: "/listImages", component: ListImages, meta: { auth: true } },
+      { path: "/manageMsg", component: ManageMessage, meta: { auth: true } },
+      { path: "/manageLink", component: ManageResLink, meta: { auth: true } },
+      { path: "/manageMusic", component: ManageMusic, meta: { auth: true } },
+    ],
+  },
+];
 
 const router = new VueRouter({
-  routes
-})
-
+  routes,
+});
+/*
 //挂载路由导航守卫,此守卫是用来拦截页面访问的，如果没有token，则不能会被重定向到登录页
 // 访问博客页面不需要token，直接放行
 // 在登陆时不需要token，直接放行
@@ -101,5 +103,6 @@ router.beforeEach((to, from, next) => {
   next();
 })
 
+*/
 
-export default router
+export default router;
