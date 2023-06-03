0e517cbd876e02c50eee1f338987d8c9567c4f8b
萱 晋 <zhangyq279@mail2.sysu.edu.cn>
2023-06-03 15:25:08 +0800

    课程具体介绍界面修改

--- vue_blog/src/components/CourseDetail.vue
+++ vue_blog/src/components/CourseDetail.vue
@@ -12,28 +12,14 @@
                 <p>考勤：{{ courseInfo.attendance }}</p>
                 <p>分数：{{ courseInfo.score }}</p>
                 <p>评价：{{ courseInfo.evaluation }}</p>
-            </div>
+            </div><hr class="hr01">
             <div>
-                <!-- 评论 -->
-                <!-- 评论栏 -->
-                <textarea class="replay" v-model="comment.content">
-                </textarea>
 
-                <b-container fluid>
-                    <b-row>
-                        <b-col sm="3">
-                            <b-button variant="outline-info" @click="publishComment">
-                                <b-icon icon="pencil-square" font-scale="0.9"></b-icon>
-                                发布评论
-                            </b-button>
-                        </b-col>
-                    </b-row>
-                </b-container>
                 <el-card>
                     <div class="comment-outer">
                         <div class="comment-body">
                             <div style="font-weight: bold">评论</div>
-                            <hr>
+                            <hr class="hr01">
                             <!-- 评论内容 -->
                             <div class="comment" :key="item.id" v-for="item in commentList">
                                 <span class="date">
@@ -46,11 +32,28 @@
                         </div>
                     </div>
                     <!-- 分页区域 -->
-                    <el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange"
+                    <el-pagination popper-class="select-down" @size-change="handleSizeChange" @current-change="handleCurrentChange"
                         :current-page="queryInfo.pageNum" :page-sizes="[10, 12, 15, 20]" :page-size="queryInfo.pageSize"
                         layout="total, sizes, prev, pager, next, jumper" :total="total">
                     </el-pagination>
                 </el-card>
+
+                <div class="text-bg">
+                    <!-- 评论栏 -->
+                    <textarea class="replay" v-model="comment.content"></textarea>
+
+                    <!-- 评论 -->
+                    <b-container fluid>
+                        <b-row>
+                            <b-col sm="3">
+                                <b-button variant="outline-info" @click="publishComment">
+                                    <b-icon icon="pencil-square" font-scale="0.9"></b-icon>
+                                    发布评论
+                                </b-button>
+                            </b-col>
+                        </b-row>
+                    </b-container>
+                </div>
             </div>
         </div>
     </div>
@@ -161,12 +164,13 @@ export default {
     }
 }
 </script>
+
 <style lang="less" scoped>
 .bg{
     background: linear-gradient(-45deg, #f5e2e0, #c2bbce, #837cb8, #3d3952);
     background-size: 300% 300%;
     animation: gradient 20s ease-in-out infinite;
-    min-height: 1600px;
+    min-height: 1700px;
 }
 @keyframes gradient {
 	0% {
@@ -179,7 +183,12 @@ export default {
 		background-position: 0% 50%;
 	}
 }
-
+.hr01{
+    margin-top: 30px;
+    border: 0;
+    height: 2px;
+    background-image: linear-gradient(to right, #c2bbce, #fef6f5, #c2bbce);
+}
 .container {
     padding-top: 80px;
     flex-direction: column;
@@ -192,46 +201,87 @@ export default {
         margin-bottom: 1em;
     }
 }
-
+// 课程信息
 .course-info {
     background-color: #fefefe;
     opacity: 0.6;
     padding: 2em;
     border-radius: 2em;
-    box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.1);
     font-size: 20px;
     p {
         margin-bottom: 1em;
         color: #3d3952;
+    }
 }
-
+.btn-outline-info{
+    margin: 20px 40px 0px 40px;
+    background-color: #c1c3db;
+    border: 0;
+    color: #565ca4;
+    border-radius: 10px;
 }
+.btn-outline-info:hover, .btn-outline-info:focus, .btn-outline-info:active{
+    border: 0 !important;
+    background-color: #565ca4 !important;
+    color: #fef6f5;
+}
+.replay {
+    width: 90%;
+    min-height: 180px;
+    background: #c1c3db;
+    opacity: 0.8;
+    margin: 30px 40px 0px 40px;
+    padding: 2em;
+    border-radius: .28571429rem;
 
+    border: 0;
+    outline: 0;
+    color: rgba(0, 0, 0, .87);
+    // transition: color .1s ease, border-color .1s ease;
+    font-size: 1em;
+    line-height: 1.2857;
+    resize: vertical;
+}
+.text-bg{
+    margin: 20px 0px 20px 0px;
+    padding: 10px;
+    background-color: #fef6f5;
+    border-radius: 1rem;
+}
 /*评论相关的样式 */
+.el-card.is-always-shadow{
+    border-radius: 1rem !important;
+}
+/deep/ .el-card__body{
+    background-color: #fef6f5;
+    
+}
 .comment-outer {
-    width: 76%;
-    margin-left: 12%;
-    margin-right: 12%;
+    width: 95%;
+    margin-left: 2.5%;
+    margin-right: 2.5%;
     border-radius: .28571429rem;
     box-shadow: none;
-    border: 1px solid #D4D4D5;
+    border: 1px solid #565ca4;
     padding: 1.5em;
 
     min-height: 300px;
-    // background: #FFF;
-    opacity: 0.9;
+    background: #ffffff;
+    opacity: 0.7;
 }
 
 .comment-body {
     position: relative;
-    // background: #FFF;
-    box-shadow: 0 1px 2px 0 rgb(34 36 38 / 15%);
+    background: #FFF;
     margin: 0.5rem 0;
     padding: 1em;
     border-radius: .28571429rem;
-    border: 1px solid rgba(34, 36, 38, .15);
-    border-top: 2px solid #00B5AD;
+    border: 2px solid #8c90c0;
     min-height: 260px;
+    hr{
+        background-image: linear-gradient(to right, #c2bbce, #c2bbce, #c2bbce);
+        height: 1px;
+    }
 
 }
 
@@ -244,21 +294,48 @@ export default {
     display: block;
 }
 
-.replay {
-    width: 100%;
-    min-height: 280px;
-    background: #fef6f5;
-    opacity: 0.8;
-    margin: 3rem 0;
-    padding: 2em;
-    border-radius: 2rem;
 
-    border: 0;
-    outline: 0;
-    color: rgba(0, 0, 0, .87);
-    // transition: color .1s ease, border-color .1s ease;
-    font-size: 1em;
-    line-height: 1.2857;
-    resize: vertical;
+/deep/ .el-pager .number:hover, .el-pager .number:active{
+    color: #393f51;
 }
+/deep/ .el-pager .number{
+    color: #565ca4;
+    font-size: 16px;
+}
+/deep/ .el-input__inner{
+    color: #565ca4;
+    font-size: 16px;
+    border: 1px solid #565ca4 !important; 
+}
+
+/deep/ .btn-prev{
+    border-radius: 4px 0 0 4px ;
+}
+/deep/ .btn-prev:hover{
+    color: #565ca4;
+}
+/deep/ .btn-next{
+    border-radius: 0 4px 4px 0;
+}
+/deep/ .btn-next:hover{
+    color: #565ca4;
+}
+.el-pagination{
+    padding-top: 30px;
+    padding-left: 270px;
+}
+</style>
+
+
+<style lang="less">
+.select-down {
+    .el-select-dropdown__item.selected{
+        color: #565ca4 !important;
+    }
+    .el-select-dropdown__item.hover, .el-select-dropdown__item:hover{
+        margin: 5px;
+    }
+	
+}
+
 </style>
