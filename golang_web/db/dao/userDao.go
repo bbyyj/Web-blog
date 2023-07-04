01071ddd0c4d47cfec87c1a98b11efb859a0d76a
MayBisCuit <jiawei03@qq.com>
2023-07-04 19:15:54 +0800

    修改 用户结构体

--- golang_web/db/dao/userDao.go
+++ golang_web/db/dao/userDao.go
@@ -10,23 +10,14 @@ func NewUserDao() *UserDao {
 	return &UserDao{
 		sql: []string{
 			`SELECT * FROM user WHERE username = ? AND password = ?;`,
-			`SELECT nickname, email, avatar, github, csdn FROM user Limit 1`,
+			`SELECT nickname, avatar FROM user Limit 1`,
 		},
 	}
 }
 
 func (u *UserDao) FindUserByUsernameAndPassword(username, password string) (*model.User, error) {
 	var user model.User
-	println("note here dao 1")
-	println(username)
-	println(password)
 	err := sqldb.Get(&user, u.sql[0], username, password)
-	println("note here dao 2")
-	println(user.Username)
-	println(user.Password)
-	// username admin
-	// password 123456
-	// password coded e10adc3949ba59abbe56e057f20f883e
 	return &user, err
 }
 
